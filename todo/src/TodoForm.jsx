be93370966cefede719f752cf778997cfaca1806
siddhantsingh186 <201951151@iiitvadodara.ac.in>
2023-02-22 09:11:10 +0530

    Added css

--- todo/src/TodoForm.jsx
+++ todo/src/TodoForm.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { addTask } from './actions';
-
+import "./TodoForm.css"
 class TodoForm extends Component {
   constructor(props) {
     super(props);
--- todo/src/TodoList.jsx
+++ todo/src/TodoList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TodoItem from './TodoItem';
-
+import "./TodoList.css"
 class TodoList extends Component {
   render() {
     const { tasks } = this.props;
